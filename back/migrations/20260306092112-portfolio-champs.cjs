fbf8c0ad4e6a10c6e08cf021f15623d1717b0bb8
João <joao@madebyjoao.fr>
2026-03-09 09:49:13 +0100

    feat: update database migrations to include user_id and additional fields in portfolio_champs and projects tables

--- back/migrations/20260306092112-portfolio-champs.cjs
+++ back/migrations/20260306092112-portfolio-champs.cjs
@@ -10,6 +10,7 @@ module.exports = {
       await q(`
         CREATE TABLE IF NOT EXISTS portfolios (
           id INT AUTO_INCREMENT PRIMARY KEY,
+          user_id INT NOT NULL,
           
 
           created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -20,7 +21,9 @@ module.exports = {
       await q(`
         CREATE TABLE IF NOT EXISTS portfolio_champs (
           id INT AUTO_INCREMENT PRIMARY KEY,
-          
+          portfolio_id INT NOT NULL,
+          about_title VARCHAR(255) NOT NULL,
+          about_text VARCHAR(255) NOT NULL,
 
           created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
           updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
@@ -30,7 +33,10 @@ module.exports = {
       await q(`
         CREATE TABLE IF NOT EXISTS projects (
           id INT AUTO_INCREMENT PRIMARY KEY,
-          
+          portfolios_id INT NOT NULL,
+          thumbnail VARCHAR(255),
+          title VARCHAR(50),
+          description VARCHAR(255),          
 
           created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
           updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
@@ -40,7 +46,9 @@ module.exports = {
       await q(`
         CREATE TABLE IF NOT EXISTS certificats (
           id INT AUTO_INCREMENT PRIMARY KEY,
-          
+          portfolio_id INT NOT NULL,
+          certificats_path VARCHAR(255),
+          certificats_info VARCHAR(255),          
 
           created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
           updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
