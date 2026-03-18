ae28a5c8c19fe1bede9ec6be90f812b0d9c65022
João <joao@madebyjoao.fr>
2026-03-18 10:52:20 +0100

    Add new image4.webp to client1-test uploads
    This commit introduces a new WebP image file named image4.webp to the client1-test uploads directory. The addition enhances the media assets available for the client, providing a new visual resource.

--- front/src/components/Certificates.jsx
+++ front/src/components/Certificates.jsx
@@ -8,8 +8,9 @@ export default function CertificatesBox({certificates_info}) {
 
         <div className="flex flex-col left-border-none bg-[rgb(234,228,213)] p-6 rounded-lg justify-center">
 
+           
+            <img className="" src={`${BASE_URL}/uploads/${certificates_info.image_path}`} alt={certificates_info.title} />
             <h1 className="font-extrabold">{certificates_info.title}</h1>
-            <img src={`${BASE_URL}/uploads/${certificates_info.image_path}`} alt={certificates_info.title} />
             <strong>{certificates_info.description}</strong>
 
         </div>
--- front/src/pages/public/Certificates.jsx
+++ front/src/pages/public/Certificates.jsx
@@ -2,11 +2,14 @@ import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router";
 import { getCertificatesBySlug } from "../../api/portfolio";
 import CertificatesBox from "../../components/Certificates";
+import { useState } from "react";
+import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 
 
 export default function Certificates() {
 
   const { slug } = useParams();
+  const [currentCertif, setCurrentCertif] = useState(0);
 
 
   const { isPending, isError, data, error } = useQuery({
@@ -27,12 +30,26 @@ export default function Certificates() {
   }
   const certificates_info = data.data.certificates;
 
+  const next = () => {
+    setCurrentCertif((prev) => (prev + 1) % certificates_info.length);
+  };
+
+  const prev = () => {
+    setCurrentCertif((prev) => (prev - 1 + certificates_info.length) % certificates_info.length)
+  };
+
   return (
 
-      <div className="flex flex-col content-center justify-center gap-2 top-50 w-1/2">
-        {certificates_info.map((certif) => (
-          <CertificatesBox  certificates_info={certif}/>
-        ))}
+      <div className="fixed flex flex-col items-center justify-center gap-4 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <button onClick={prev} className="absolute left-0 z-10" aria-label="Previous Certificate">
+          <ChevronLeftIcon size={50} strokeWidth={4}/>
+        </button>
+        
+        <CertificatesBox  certificates_info={certificates_info[currentCertif]} />
+
+        <button onClick={prev} className="absolute right-0 z-10" aria-label="Next Certificate">
+          <ChevronRightIcon size={50} strokeWidth={4}/>
+        </button>
       </div>
 
   )
