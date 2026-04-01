db4a59ac6c8296aa7f2327d7fc8097fc85167351
madebyjoao <joao@madebyjoao.fr>
2026-04-01 17:41:27 +0200

    feat: enhance portfolio component with improved loading and error handling for project data

--- front/src/pages/public/Portfolio.jsx
+++ front/src/pages/public/Portfolio.jsx
@@ -8,17 +8,38 @@ import TemplateTwo from "../../components/templates/TemplateTwo";
 export default function Portfolio() {
   const { slug } = useParams();
 
-  const { isPending, isError, data, error } = useQuery({
+  const { 
+    isPending: isPortfolioPending, 
+    isError: isPortfolioError, 
+    data: portfolioData, 
+    error: portfolioError, 
+  } = useQuery({
     queryKey: ["portfolio", slug],
     queryFn: () => getPortfolioBySlug(slug),
     enabled: !!slug,
   });
 
-  if (isPending) {
+  const { 
+    isPending: isProjectsPending, 
+    isError: isProjectsError, 
+    data: projectsData, 
+    error: projectsError, 
+  } = useQuery({
+    queryKey: ["projects", slug],
+    queryFn: () => getProjectsBySlug(slug),
+    enabled: !!slug,
+  });
+
+  const isLoading = isPortfolioPending || isProjectsPending;
+
+
+  if (isLoading) {
     return <div>Chargement en cours...</div>;
   }
 
-  if (isError) {
+  const isMistake = isPortfolioError || isProjectsError;
+
+  if (isMistake) {
     return <div>Erreur : {error.message}</div>;
   }
 
